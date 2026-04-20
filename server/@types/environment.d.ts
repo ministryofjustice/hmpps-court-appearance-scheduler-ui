dd97a7a5ba7734f8020401cd875db3b5fb880570
bill-moj <167539377+bill-moj@users.noreply.github.com>
2026-04-20 11:46:49 +0100

    Apply strict type rules (#2)

---
+++ server/@types/environment.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: string
+      PORT?: string
+      REDIS_PORT: string
+      REDIS_AUTH_TOKEN: string
+      APPLICATIONINSIGHTS_CONNECTION_STRING: string
+      NO_HTTPS?: string
+      CI?: string
+    }
+  }
+}
+
+export {}
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from 'express'
 
 import type { Services } from '../services'
 import { Page } from '../services/auditService'
+import breadcrumbs from '../middleware/breadcrumbs'
 
 export default function routes({ auditService }: Services): Router {
   const router = Router()
 
-  router.get('/', async (req, res, next) => {
+  router.use(breadcrumbs())
+
+  router.get('/', async (req, res) => {
     await auditService.logPageView(Page.HOMEPAGE, { who: res.locals.user.username, correlationId: req.id })
     return res.render('view', { showBreadcrumbs: true, currentTime: new Date() })
   })
