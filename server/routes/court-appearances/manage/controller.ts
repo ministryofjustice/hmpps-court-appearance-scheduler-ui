0be93d7917fb4e7401f5245056d6a21464e5e679
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-06-17 12:07:16 +0100

    Handle court appearances from RaS

--- server/routes/court-appearances/manage/controller.ts
+++ server/routes/court-appearances/manage/controller.ts
@@ -7,11 +7,14 @@ export class ManageCourtAppearanceController {
   constructor(private readonly courtRegisterService: CourtRegisterService) {}
 
   GET = async (req: Request, res: Response) => {
+    const isFromRAS = req.middleware!.courtAppearance!.origin?.source.code === 'remand-and-sentencing'
+
     res.render('court-appearances/manage/view', {
       showBreadcrumbs: true,
       courtAppearance: req.middleware!.courtAppearance,
+      sourceName: req.middleware!.courtAppearance!.origin?.source.name,
       editable: isCourtAppearanceEditable(req.middleware!.courtAppearance!),
-      cancellable: req.middleware!.courtAppearance!.status.code === 'SCHEDULED',
+      cancellable: req.middleware!.courtAppearance!.status.code === 'SCHEDULED' && !isFromRAS,
       auditedActions: parseAuditHistory(
         req.middleware!.appearanceHistory!.content.sort((a, b) => b.occurredAt.localeCompare(a.occurredAt)),
         await this.courtRegisterService.getCourts({ res }),
