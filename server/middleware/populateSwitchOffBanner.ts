8373ef3ca45bd4a380efe38779cfaf2ab93b411e
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-06-22 18:14:01 +0100

    Address PR comments

--- server/middleware/populateSwitchOffBanner.ts
+++ server/middleware/populateSwitchOffBanner.ts
@@ -13,7 +13,7 @@ export const populateSwitchOffBanner = (_req: Request, res: Response, next: Next
     }
   } else {
     res.locals.switchOffBanner = {
-      html: '<p>  On 13 July, we will be rolling out court appearances on DPS. You will be able to:</p><ul class="govuk-list govuk-list--bullet"><li>create court appearances</li><li>view and manage court appearances</li><li>view a prisoner\'s court appearance history</li></ul><p>Staff with View only or Management roles assigned by their Local System Administrator (LSA) can access the service. Please request this directly with your LSA. Guidance and access information can be found on our <a class="govuk-link" target="_blank" href="https://justiceuk.sharepoint.com/:u:/r/sites/prisons-digital/SitePages/External%20Movements%20-%20Court%20Scheduling.aspx?csf=1&web=1&e=WqkBgc">Courts SharePoint page</a>.</p>',
+      html: '<p>On 13 July, we will be rolling out court appearances on DPS. You will be able to:</p><ul class="govuk-list govuk-list--bullet"><li>create court appearances</li><li>view and manage court appearances</li><li>view a prisoner\'s court appearance history</li></ul><p>Staff with View only or Management roles assigned by their Local System Administrator (LSA) can access the service. Please request this directly with your LSA. Guidance and access information can be found on our <a class="govuk-link" target="_blank" href="https://justiceuk.sharepoint.com/:u:/r/sites/prisons-digital/SitePages/External%20Movements%20-%20Court%20Scheduling.aspx?csf=1&web=1&e=WqkBgc">Courts SharePoint page</a>.</p>',
     }
   }
 
