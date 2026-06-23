9399ffcb26197a1ddc31924d54208b2cf4ba5936
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-06-23 08:54:21 +0100

    Address PR comments

--- server/middleware/populateSwitchOffBanner.ts
+++ server/middleware/populateSwitchOffBanner.ts
@@ -7,7 +7,7 @@ export type SwitchOffBanner = {
 
 export const populateSwitchOffBanner = (_req: Request, res: Response, next: NextFunction) => {
   const dateString = format(new Date(), 'yyyy-MM-dd')
-  if (dateString >= '2026-07-14') {
+  if (dateString >= '2026-07-13') {
     res.locals.switchOffBanner = {
       html: '<p>Court appearances are now available on DPS. You can:</p><ul class="govuk-list govuk-list--bullet"><li>create court appearances</li><li>view and manage court appearances</li><li>view a prisoner\'s court appearance history</li></ul><p>Staff with View only or Management roles assigned by their Local System Administrator (LSA) can access the service. Please request this directly with your LSA. Guidance and access information can be found on our <a class="govuk-link" target="_blank" href="https://justiceuk.sharepoint.com/:u:/r/sites/prisons-digital/SitePages/External%20Movements%20-%20Court%20Scheduling.aspx?csf=1&web=1&e=WqkBgc">Courts SharePoint page</a>.</p>',
     }
