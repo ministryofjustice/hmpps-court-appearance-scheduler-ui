b65bd490a52d140c786b1456593e612774addd2c
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-08-24 16:53:30 +0100

    Add print view for browse court appearances page

--- assets/js/hmpps-sort-selector.js
+++ assets/js/hmpps-sort-selector.js
@@ -1,8 +1,7 @@
 export const initHmppsSortSelector = () => {
-  const $button = document.querySelector('.hmpps-sort-selector__sort-button')
-  if ($button) {
-    $button.classList.add('govuk-!-display-none')
-
+  const $buttons = Array.from(document.querySelectorAll('.hmpps-sort-selector__sort-button'))
+  if ($buttons.length) {
+    $buttons.forEach($button => $button.classList.add('govuk-!-display-none'))
     Array.from(document.getElementsByClassName('hmpps-sort-selector__select')).forEach(s => {
       s.addEventListener('change', () => {
         s.form.submit()
